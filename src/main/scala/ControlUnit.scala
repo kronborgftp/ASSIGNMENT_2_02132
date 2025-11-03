46f16a274c3c20c5fdabbd4e3b84f73e2833df23
kronborgftp <s236369@dtu.dk>
2025-11-03 08:11:02 +0100

    Task 8 done i think.....

--- src/main/scala/ControlUnit.scala
+++ src/main/scala/ControlUnit.scala
@@ -1,11 +1,95 @@
 import chisel3._
 import chisel3.util._
 
+/*
+ * Instruction layout:
+ * [15:12] opcode
+ * [11:8]  rd
+ * [7:4]   rs
+ * [3:0]   rt
+ */
 class ControlUnit extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val instr = Input(UInt(16.W))
+
+    // register file interface
+    val rdSel = Output(UInt(4.W))
+    val rsSel = Output(UInt(4.W))
+    val rtSel = Output(UInt(4.W))
+    val regWrite = Output(Bool())
+
+    // ALU
+    val aluSel = Output(UInt(4.W))
+
+    // memory
+    val memWrite = Output(Bool())
+    val useMemData = Output(Bool()) // 1 => write back data from memory (LD)
+
+    // program counter
+    val pcJump = Output(Bool())
+    val pcStop = Output(Bool())
   })
 
-  //Implement this module here
+  //  decode fields
+  val opcode = io.instr(15,12)
+  val rd     = io.instr(11,8)
+  val rs     = io.instr(7,4)
+  val rt     = io.instr(3,0)
 
+  //defaults
+  io.rdSel := rd
+  io.rsSel := rs
+  io.rtSel := rt
+
+  io.regWrite := false.B
+  io.aluSel := 0.U
+  io.memWrite := false.B
+  io.useMemData := false.B
+  io.pcJump := false.B
+  io.pcStop := false.B
+
+  //  main decode
+  switch(opcode) {
+    is("b0000".U) { // ADD
+      io.aluSel := "b0000".U; io.regWrite := true.B
+    }
+    is("b0001".U) { // SUB
+      io.aluSel := "b0001".U; io.regWrite := true.B
+    }
+    is("b0010".U) { // OR
+      io.aluSel := "b0010".U; io.regWrite := true.B
+    }
+    is("b0011".U) { // AND
+      io.aluSel := "b0011".U; io.regWrite := true.B
+    }
+    is("b0100".U) { // NOT
+      io.aluSel := "b0100".U; io.regWrite := true.B
+    }
+    is("b0101".U) { // GT
+      io.aluSel := "b0101".U; io.regWrite := true.B
+    }
+    is("b0110".U) { // MOD
+      io.aluSel := "b0110".U; io.regWrite := true.B
+    }
+
+    // memory operations
+    is("b1000".U) { // LD  rd = mem(rs)
+      io.useMemData := true.B
+      io.regWrite := true.B
+    }
+    is("b1001".U) { // SD  mem(rd) = rs
+      io.memWrite := true.B
+    }
+
+    // control
+    is("b1010".U) { // GO rd = address, rs = condition
+      io.pcJump := true.B // PC jump will be conditioned in top-level
+    }
+    is("b1110".U) { // NOP
+      // do nothing
+    }
+    is("b1111".U) { // STOP
+      io.pcStop := true.B
+    }
+  }
 }
