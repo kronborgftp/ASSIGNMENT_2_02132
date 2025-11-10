e12b7137179401458623fbb67decd52655139971
kronborgftp <s236369@dtu.dk>
2025-11-10 09:28:16 +0100

    Load  Immediate added. Updated all classes according to update OPCode table

--- src/test/scala/ALUTester.scala
+++ src/test/scala/ALUTester.scala
@@ -20,7 +20,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 10 + 3 = 13
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0000".U)  // ADD opcode
+      dut.io.sel.poke("b0001".U)  // ADD opcode
       dut.clock.step(1)
       dut.io.out.expect(13.U)
       println("ADD test passed (10 + 3 = 13)")
@@ -29,7 +29,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 10 - 3 = 7
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0001".U)  // SUB opcode
+      dut.io.sel.poke("b0010".U)  // SUB opcode
       dut.clock.step(1)
       dut.io.out.expect(7.U)
       println("SUB test passed (10 - 3 = 7)")
@@ -38,7 +38,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 10 (1010) | 3 (0011) = 11 (1011)
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0010".U)  // OR opcode
+      dut.io.sel.poke("b0011".U)  // OR opcode
       dut.clock.step(1)
       dut.io.out.expect((10 | 3).U)
       println("OR test passed (10 | 3 = 11)")
@@ -47,7 +47,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 10 (1010) & 3 (0011) = 2 (0010)
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0011".U)  // AND opcode
+      dut.io.sel.poke("b0100".U)  // AND opcode
       dut.clock.step(1)
       dut.io.out.expect((10 & 3).U)
       println("AND test passed (10 & 3 = 2)")
@@ -57,7 +57,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 65525 (1111 1111 1111 0101)
       dut.io.a.poke(10.U)
       dut.io.b.poke(0.U)             // b unused in NOT
-      dut.io.sel.poke("b0100".U)     // NOT opcode
+      dut.io.sel.poke("b0101".U)     // NOT opcode
       dut.clock.step(1)
       dut.io.out.expect(65525.U)     // expected 16-bit inverted value
       println("NOT test passed (~10 = 65525)")
@@ -66,7 +66,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: (10 > 3) ? 1 : 0  -> should return 1
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0101".U)  // GT opcode
+      dut.io.sel.poke("b0110".U)  // GT opcode
       dut.clock.step(1)
       dut.io.out.expect(1.U)
       println("GT test passed (10 > 3 -> 1)")
@@ -75,7 +75,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Expected: 10 % 3 = 1
       dut.io.a.poke(10.U)
       dut.io.b.poke(3.U)
-      dut.io.sel.poke("b0110".U)  // MOD opcode
+      dut.io.sel.poke("b0111".U)  // MOD opcode
       dut.clock.step(1)
       dut.io.out.expect((10 % 3).U)
       println("MOD test passed (10 % 3 = 1)")
@@ -84,7 +84,7 @@ class ALUTester extends AnyFlatSpec with ChiselScalatestTester {
       // Test the zero flag output by setting a = b (so result = 0)
       dut.io.a.poke(5.U)
       dut.io.b.poke(5.U)
-      dut.io.sel.poke("b0001".U)  // SUB (5 - 5 = 0)
+      dut.io.sel.poke("b1111".U)  // SUB (5 - 5 = 0)
       dut.clock.step(1)
       dut.io.out.expect(0.U)
       dut.io.zero.expect(true.B)
